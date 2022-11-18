1a7fee53b100fab8fe552fa000d0cdeea807883c
Potapov Vladislav <89172111013q@gmail.com>
2022-11-18 23:37:44 +0300

    Добавление модального окна для удаления контакта

--- js/activeBtn.js
+++ js/activeBtn.js
@@ -1,5 +1,5 @@
 const modalAdd = document.querySelector('#btnAdd')
-const modal = document.querySelector('.modal');
+const modal = document.querySelector('#createClient');
 const modalClose = document.querySelector('.modal__close-btn');
 
 modalAdd.addEventListener('click', function (e) {
@@ -12,4 +12,11 @@ modalClose.addEventListener('click', function(e) {
   modal.classList.remove('active')
 })
 
+const openDeleteModal = document.querySelector('#openDeleteModal');
+const deleteClient = document.querySelector('#deleteClient');
 
+openDeleteModal.addEventListener('click', function (e) {
+  e.preventDefault()
+  modal.classList.remove('active')
+  deleteClient.classList.add('active')
+})
